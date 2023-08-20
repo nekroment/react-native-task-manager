90c1781b5ca05e1cd726d86297dcfb75f6687159
Pavel Adatsev <adamcevs@gmail.com>
2023-08-20 15:28:05 +0300

    added base pages

---
+++ pages/SignUpPage/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native';
+import { FC } from 'react';
+
+import { NavigationNames, NavigationParamList } from '../../resources/types';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+
+import styles from "./style";
+
+type Props = NativeStackScreenProps<NavigationParamList, NavigationNames.START>;
+
+export const SignUpPage: FC<Props> = ({ navigation }) => {
+    return (
+        <View style={styles.wrapper}>
+            <Text>Sign Up Page</Text>
+        </View>
+    )
+}
